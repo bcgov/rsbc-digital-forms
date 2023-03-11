929b8554e577e4f6e9444e688424693d1f8e57d1
NavpreetGrewal <ngrewal002@gmail.com>
2023-03-10 18:10:07 -0700

    Initial Commit for the React Application

---
+++ frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['js', 'jsx', 'node', 'json'],
+    transform: {
+      '^.+\\.(js|jsx?)$': 'babel-jest',
+    },
+  };
+  
