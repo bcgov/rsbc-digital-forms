5a88044e9709e735ca948b9d9da4e4053dbe4998
NavpreetGrewal <ngrewal002@gmail.com>
2023-03-14 17:22:07 -0600

    Change react version from 18 to 17 and other related dependency versions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
