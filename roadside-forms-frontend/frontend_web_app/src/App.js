37d0487449636fa99856ece6be87072c1281da74
JustinMacaulay <justinkmacaulay@gmail.com>
2023-03-28 10:54:07 -0700

    added in keycloak packages and started to build out components to add in keycloak auth

--- roadside-forms-frontend/frontend_web_app/src/App.js
+++ roadside-forms-frontend/frontend_web_app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import Router from './utils/router';
+import keycloak from './keycloak';
 
 function App() {
   return (
-    <div className="App">
-      <Router/>
+    <div className="App">    
+      {/* <ReactKeycloakProvider authClient={keycloak}> */}
+        <Router/>
+      {/* </ReactKeycloakProvider> */}
     </div>
   );
 }
