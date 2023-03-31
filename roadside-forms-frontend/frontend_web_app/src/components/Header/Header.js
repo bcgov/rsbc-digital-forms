f0a6bce53706aaa47bd00668b94de72539c43b41
NavpreetGrewal <ngrewal002@gmail.com>
2023-03-31 12:59:57 -0600

    add more elements to the header

--- roadside-forms-frontend/frontend_web_app/src/components/Header/Header.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Header/Header.js
@@ -1,32 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Button } from '../Button/Button';
+import { useState, useEffect } from 'react';
 import './header.css';
+import { getCurrentDateTime } from '../../utils/dateTime';
 
-export const Header = ({ user, onLogin, onLogout}) => (
+export const Header = ({ user, onLogin, onLogout}) => {
+  const [isConnected, setIsConnected] = useState(navigator.onLine);
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
+  const [day, setDay] = useState('');
+
+  useEffect(() => {
+    const handleOnline = () => setIsConnected(true);
+    const handleOffline = () => setIsConnected(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    const interval = setInterval(() => {
+      const {dateString, dayString, timeString} = getCurrentDateTime();
+      setDate(dateString);
+      setDay(dayString + ',');
+      setTime(timeString);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    }
+  }, []);
+
+  return (
   <header>
     <div id="roadsafety-header" className='container'>
       <div className='row'> 
-        <div className="col-sm-4" >
+        <div className="col-sm-3" >
           <div className='brand-logo'></div>
         </div>
-        <div className="col-sm-8 col-right">
-          <div className="font-weight-bold text-warning mb-4">
-            Version&nbsp;<span className="text-light small" id="app-version">-&nbsp;0.1</span>
+        { user && (<div className='col-sm-9'>
+        <div className="row">
+          <div className=" col-sm-4 time font-weight-bold mt-4">
+            &nbsp;<span className="text-light d-block large">{time}</span>
+            <span className="text-light d-block large">{day} {date}</span>
           </div>
-          { user &&  ( 
-          <div className="mt-auto">
-            &nbsp;<span className="text-light large">Navpreet Sidhu</span>
-            &nbsp;&nbsp;&nbsp;
-            <Button  size='small' primary label="Sign Out"/>
+          <div className=" col-sm-2 icon font-weight-bold mt-4">
+            {isConnected ? (
+              <div className='connected'></div>
+            ) : (
+              <div className='disconnected'></div>
+            )}
           </div>
-          )}
-        </div>
+          <div className=" col-sm-4 user-info font-weight-bold col-right">
+            &nbsp;<span className="text-light d-block large">John SMITH</span>
+            <span className="text-light d-block large">Vancouver Police Dept.</span>
+          </div>
+          <div className=" col-sm-2 links font-weight-bold col-right">
+            &nbsp;<a className="d-block text-light" href="https://example.com">Settings</a>
+            <a className="d-block text-light" href="https://example.com">Logout</a>
+          </div>
+         </div>
+         </div>
+         )}
       </div>
     </div>
   </header>
 );
+}
 
 Header.propTypes = {
   user: PropTypes.shape({}),
