d3ea30b418a12636277379f31339cb66de753b1b
NavpreetGrewal <ngrewal002@gmail.com>
2023-04-20 15:35:29 -0600

    Buidl new user application flow initial commit

--- roadside-forms-frontend/frontend_web_app/src/utils/router.js
+++ roadside-forms-frontend/frontend_web_app/src/utils/router.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import { Login } from '../components/Login/login';
-// import PrivateRoute from '../routes/PrivateRoute';
+import { Dashboard } from '../components/Dashboard/Dashboard';
+import PrivateRoute from '../routes/PrivateRoute';
 import { RequestAccess } from '../components/RequestAccess/requestAccess';
 
 class Router extends Component {
     render() {
     return (
         <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<Login/>} />
-                    <Route path="/requestAccess" element={<RequestAccess/>}  />
-                    <Route path="/" element={<Navigate to="/login" />} />
-                </Routes>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
+                <Route path="/requestAccess" element={<RequestAccess />}  />
+            </Routes>
         </BrowserRouter>
     );
   }
