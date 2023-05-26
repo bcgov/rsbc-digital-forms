b12092e3a38cf6cc4387fb367b64c50af048b649
NavpreetGrewal <ngrewal002@gmail.com>
2023-05-26 09:22:17 -0600

    24 Hour form specific fields and minor fixes

---
+++ roadside-forms-frontend/frontend_web_app/src/components/common/Input/NumericInput.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useField } from 'formik';
+
+export const NumericInput = ({ required, label, name, ...props }) => {
+  const [field, meta] = useField(name);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    // Limit the input value to 999
+    const sanitizedValue = value.replace(/[^0-9]/g, '').slice(0, 3);
+
+    // Update the field value
+    field.onChange({ target: { name, value: sanitizedValue } });
+  };
+
+  return (
+    <div>
+      <label htmlFor={name}>{label}{required && <span className="required-asterisk"> *</span>}</label>
+      <input
+        type="text"
+        id={name}
+        {...field}
+        {...props}
+        onChange={handleChange}
+        required={required}
+        className={`form-control ${meta.touched && meta.error && 'is-invalid'}`}
+      />
+      {meta.touched && meta.error && (
+        <div className="invalid-feedback">{meta.error}</div>
+      )}
+    </div>
+  );
+};
