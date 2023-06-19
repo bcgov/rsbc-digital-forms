ad629445b20eb68dd49b27c5242146a4c8aca087
NavpreetGrewal <ngrewal002@gmail.com>
2023-06-19 16:47:28 -0600

    Defect Fixes for forms

--- roadside-forms-frontend/frontend_web_app/src/components/Forms/TwentyFourHourForm/twentyFourHourForm.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Forms/TwentyFourHourForm/twentyFourHourForm.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { VehicleImpoundment } from './vehicleImpoundment';
 import { Prohibition } from './prohibition';
 import { ReasonableGrounds } from './reasonableGrounds';
+import { TestAdministered } from './testAdministered';
 import PropTypes from 'prop-types';
 
 export const TwentyFourHourForm = (props) => {
-    const {cities, impoundLotOperators} = props;
-    return (
-      <div>
-      <VehicleImpoundment impoundLotOperators={impoundLotOperators}/>
-      <Prohibition cities={cities}/>
-      <ReasonableGrounds/> 
-      </div>
-    );
+  const {cities, impoundLotOperators} = props;
+  return (
+    <div>
+    <VehicleImpoundment impoundLotOperators={impoundLotOperators}/>
+    <Prohibition cities={cities}/>
+    <ReasonableGrounds/>
+    <TestAdministered/>
+    </div>
+  );
 }
 
 TwentyFourHourForm.propTypes = {
