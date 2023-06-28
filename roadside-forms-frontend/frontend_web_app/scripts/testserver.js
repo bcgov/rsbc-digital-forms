94dae077a2d16ab2ced47bfaf3beaba5885640e4
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-06-28 10:46:05 -0700

    updated

--- roadside-forms-frontend/frontend_web_app/scripts/testserver.js
+++ roadside-forms-frontend/frontend_web_app/scripts/testserver.js
@@ -6,6 +6,7 @@ const publicPath=path.join(__dirname,'../build')
 
 
 const app=express()
+app.disable("x-powered-by")
 app.use(express.static(publicPath))
 app.listen(port,()=>{
     console.log(`Server started at ${port}`)
