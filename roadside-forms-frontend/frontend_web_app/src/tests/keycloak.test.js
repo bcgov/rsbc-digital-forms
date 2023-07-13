7e946a8f285664879947791b6865b20ce602838b
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-07-13 13:49:27 -0700

    added

--- roadside-forms-frontend/frontend_web_app/src/tests/keycloak.test.js
+++ roadside-forms-frontend/frontend_web_app/src/tests/keycloak.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { Header } from '../components/common/Header/Header';
 import { useKeycloak } from '@react-keycloak/web';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('@react-keycloak/web');
 
@@ -14,7 +15,7 @@ describe('Keycloak test', () => {
       initialized: true,
     });
 
-    const { getByTestId } = render(<Header />);
+    const { getByTestId } = render(<BrowserRouter><Header /></BrowserRouter>);
     expect(getByTestId('roadsafety-header')).toBeInTheDocument();
   });
 });
