e265efa0beef2f617dcced925fb659db0371a47d
JustinMacaulay <justinkmacaulay@gmail.com>
2023-07-20 11:08:24 -0700

    removed console.log statements.

--- roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
@@ -188,7 +188,6 @@ export const CreateEvent = () => {
       const renderSVGForm = (values) => {
         const forms = {"TwentyFourHour": values["24Hour"], "TwelveHour": values["12Hour"], "IRP": values["IRP"], "VI": values["VI"] }
         const componentsToRender = []
-        console.log(values)
         for(const item in forms){
             if (forms[item]) {
                 for (const form in formsPNG[item]) {
