6f22237354e6064cd7de81c91a5fdc614a64430a
JustinMacaulay <justinkmacaulay@gmail.com>
2023-08-21 15:11:20 -0700

    missed duplicate import

--- roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
 import Row from 'react-bootstrap/Row';
-import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal'
 import { Checkbox } from '../common/Checkbox/checkbox';
