8f65b28036b2dd99b42b035478a45b8ebbe7796d
JustinMacaulay <justinkmacaulay@gmail.com>
2023-09-13 12:37:07 -0700

    VI data entry and print layout

--- roadside-forms-frontend/frontend_web_app/src/components/Forms/TwentyFourHourForm/vehicleImpoundment.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Forms/TwentyFourHourForm/vehicleImpoundment.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
-import { Radio } from '../../common/Radio/radio';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useFormikContext} from 'formik';
+import PropTypes from 'prop-types';
+import { Radio } from '../../common/Radio/radio';
 import { Input } from '../../common/Input/Input';
 import { DatePickerField } from '../../common/DateField/DatePicker';
 import { TimeInputField } from '../../common/Input/TimeInputField';
 import { PhoneField } from '../../common/Input/phoneField';
 import { SearchableSelect } from '../../common/Select/SearchableSelect';
-import PropTypes from 'prop-types';
+
 
 export const VehicleImpoundment = (props) => {
     const {impoundLotOperators} = props;
@@ -28,6 +31,9 @@ export const VehicleImpoundment = (props) => {
     };
 
     useEffect(() => {
+        if (values['VI']) {
+            setFieldValue('vehicle_impounded', 'YES')
+        }
         if (values['vehicle_impounded'] === 'NO') {
           setFieldValue('key-location', '');
           setFieldValue('ILO-name', '');
@@ -39,7 +45,7 @@ export const VehicleImpoundment = (props) => {
         else if(values['vehicle_impounded'] === 'YES'){
           setFieldValue('reason_for_not_impounding', '')
         }
-      }, [values['vehicle_impounded'], setFieldValue]);
+      }, [values['vehicle_impounded'], values['VI'], setFieldValue]);
 
 
     const reasonForNotImpounding = [
@@ -51,69 +57,79 @@ export const VehicleImpoundment = (props) => {
     return(
         <div className='border-design-form left text-font'>
             <h3 >Vehicle Impoundment or Dispostion</h3>
-            <div className="row">
-                <div className="col">
+            {!values['VI'] &&
+            <Row>
+                <Col>
                     <Radio label="Vehicle Impounded?" name="vehicle_impounded" options={[
                     { label: 'Yes', value: 'YES' },
                     { label: 'No', value: 'NO' }
                     ]} required />
-                </div>
-            </div>
-        {values['vehicle_impounded'] === 'YES' && (<>
-            <div className="row">
-                <div className="col">
+                </Col>
+            </Row>
+            }
+        {values['VI'] &&
+        <Row>
+            <Col>
+                <DatePickerField name="date_of_impound" label="Date of vehicle impound" className="field-height field-width" required/>
+            </Col>
+        </Row>
+        }
+        {values['24Hour'] && values['vehicle_impounded'] === 'YES' && (<>
+            <Row>
+                <Col>
                     <Radio label="Location of Keys?" name="location_of_keys" options={[
                         {value:"WITH VEHICLE", label:"With vehicle"},
                         {value:"WITH DRIVER", label:"With driver"}
                     ]} required />
-                    
-                </div>
-                </div>
+                </Col>
+            </Row>
+            </>)}
+        {((values['24Hour'] && values['vehicle_impounded'] === 'YES') || values['VI'])  && (<>
             <div className='impound-lot-operator'>
-                <div className='row'>
-                <div className="col-sm-12">
-                    <SearchableSelect  onChange={handleILOChange} className='field-height field-width' label="Search for Impound Lot Operator"  name="ILO-options" options={impoundLotOperators}/>
-                </div>
-                </div>
+                <Row>
+                    <Col sm={12}>
+                        <SearchableSelect  onChange={handleILOChange} className='field-height field-width' label="Search for Impound Lot Operator"  name="ILO-options" options={impoundLotOperators}/>
+                    </Col>
+                </Row>
                 <div className='impoud-lot-fields' style={{backgroundColor: "lightgray"}}>
-                    <div className='row'>
-                        <div className='col-sm-12'>
+                    <Row>
+                        <Col sm={12}>
                             <Input value={values['ILO-name']} label="Impound Lot Operator Name" name="ILO-name"  className="field-height field-width" type="text" required/>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-sm-4'>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={4}>
                             <Input value={values['ILO-address']} label="Public Lot Address" name="ILO-address"  className="field-height field-width" type="text" required/>
-                        </div>
-                        <div className='col-sm-4'>
+                        </Col>
+                        <Col sm={4}>
                             <Input value={values['ILO-city']} label="City" name="ILO-city"  className="field-height field-width" type="text" required/>
-                        </div>
-                        <div className='col-sm-4'>
+                        </Col>
+                        <Col sm={4}>
                             <PhoneField value={values['ILO-phone']} className="field-height field-width" label="Public Phone Number" name="ILO-phone" required/>
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
                 </div>
             </div>
         </>)}
         {values['vehicle_impounded'] === 'NO' && (
         <>
-            <div className="row" style={{ minHeight: '85px' }}>
-                <div className="col">
+            <Row style={{ minHeight: '85px' }}>
+                <Col>
                     <Radio label="Reason for not impounding?" name="reason_for_not_impounding" options={reasonForNotImpounding} required/>
-                </div>
-            </div>
+                </Col>
+            </Row>
             {values['reason_for_not_impounding'] === 'released' && (
-                <div className='row' style={{ minHeight: '85px' }}>
-                    <div className='col-sm-4'>
+                <Row style={{ minHeight: '85px' }}>
+                    <Col sm={4}>
                         <Input label="Vehicle Released To" name="vehicle_released_to"  className="field-height field-width" type="text" required/>
-                    </div>
-                    <div className='col-sm-4'>
+                    </Col>
+                    <Col sm={4}>
                         <DatePickerField name="date_released" label="Date Released" className="field-height field-width" required/>
-                    </div>
-                    <div className='col-sm-4'>
+                    </Col>
+                    <Col sm={4}>
                         <TimeInputField label="Time" className="field-height field-width" name="time_released" required/>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             )}
         </>)}
     </div>
