c702a9036c4db783a2068366a8774b4e3e112231
Forsyth, Steve PSSG:EX <steve.1.forsyth@gov.bc.ca>
2023-09-27 08:08:48 -0700

    Update ids in form elements, add new fields.

--- qa/extension/README.md
+++ qa/extension/README.md
@@ -1,35 +1,44 @@
 # Roadside forms automation browser extension
-*A minimal extension to make filling out roadside digital forms easier.*
+*A minimal browser extension to make filling out roadside digital forms easier.*
 
 ## Description
-When you install this extension in your browser and go to the Digital Forms DEV, TEST, or localhost environments, a "Fill" button appar in the top-left corner of the page. Click it to fill out the fields quickly with random data.
+When you install this extension in your browser and go to the Digital Forms DEV or TEST environment, a "Fill" button will be added to the top-left corner of the page. After you have selected a 24-hour form, click the Fill button to fill out the fields quickly.
 
 ## Installation
-To install this extension, you must be using a Chromium-based browser like Edge or Chrome. Go to **Settings** > **Extensions** and enable **Developer Mode**. You should then see the option **Load Unpacked** appear. Click it and select the folder where this file is checked out. 
+To install this extension, you must be using a Chromium-based browser like Edge or Chrome. Go to Settings > Extensions and enable developer mode. After enabling developer mode, you should see the option "Load Unpacked" appear. Click "Load Unpacked" and select the folder where this file is located. 
 
-You may wish to pin the extension so its icon always visible. Or, not. The icon will show in colour and the Fill button will appear only when the DEV, TEST, or localhost environments are loaded.
+You may wish to pin the extension so it is always visible. The icon will show in colour and the Fill button will appear only when the DEV or TEST environments are loaded.
 
-Steps to sideload this extension:
- 1. Go to **Extensions** in the browser settings.
- 2. Enable the **Developer Mode** toggle (this allows you to side-load the extension).
- 3. Select the **Load Unpacked** link on the Extensions page.
- 4. Enable the extension, if needed.
- 5. Browse to the DEV or TEST environment.
+Steps:
+ 1. Go to Extensions.
+ 2. Enable developer mode.
+ 3. Select "Load Unpacked" on the extensions page.
+ 4. Enable the extension.
 
-This screenshot illustrates the steps:
 ![Steps to install this extension](images/installation.png)
 
 ## Usage
 
-When in the DEV, TEST, or local environments, click the "Fill" button in the top-left corner of the page:
+When in the DEV or TEST environment, click the "Fill" button in the top-left corner of the page:
 
 ![This is what the button looks like](images/usage.png)
 
 Example of the form after clicking the "Fill" button:
 
-<img src="images/example.gif">
+![Filled form](example.png)
 
+## Making changes to this extension
+If you want to modify this project, here is a description of what each file does:
 
-## Also see
+- icons/: folder for icon images shown in the extension bar.
+- chance.min.js: copy of the open-source chance.js library, for generating field values.
+- content.js: the primary script, which gets called when the Fill button is pressed.
+- helper-functions.js: helper functions called from content.js.
+- tables.js: data used from content.js (e.g. a table of towns and cities in BC).
+- manifest.json: the file that the browser uses to get information about this extension
+- README.md: this file
 
-If you want to learn more about how this extension works, and how to update it, see [DEVELOPER.md](DEVELOPER.md).
+## Notes
+This extension works by injecting JavaScript onto pages that load from the DEV and TEST environment. The JavaScript accesses the form elements (fields, buttons, checkboxes) and adds values to the fields. The custom React fields may not work as expected until you click on the selected values. This should be fixable in the near future if we can figure out how these custom elements work.
+
+The extension should work in any Chromium-based browser. It will not work in Firefox unless you change the manifest to v2.
--- qa/extension/content.js
+++ qa/extension/content.js
@@ -1,87 +1,10 @@
 /*
+Form-filler automation for DF 2.0 roadside forms.
 This code runs when a page from the RSF DEV or TEST server is loaded.
 */
+
 // Element id to use for the automation menu and buttons div
 let automationId = "qa-automation";
-
-
-// Helper function to put a button on the form, so you can click it to fill the form
-function AddButton(buttonName, topLocation, leftLocation, zIndex, fieldStructure) 
-{
-    let btn = document.createElement("button");
-    btn.innerHTML = buttonName;
-    buttonStyle = "top:" + topLocation + " !important;left:" + leftLocation + " !important;position:fixed;z-index: " + zIndex;
-    console.log("Adding button " + buttonName + ": " + buttonStyle);
-    btn.style = buttonStyle;
-
-    btn.addEventListener('click', () => {
-        // Driver's information section
-        SetField('drivers-number', GenerateDL());
-        //let licenceJurisdictions = ValuesFromDropdownField('react-select-2-input');
-        //SetField('react-select-2-input', licenceJurisdictions[Math.floor(Math.random() * licenceJurisdictions.length)]);
-        SetField('last-name', chance.last());
-        SetField('given-name', GenerateGivenNames());
-        SetField('dob', GenerateDateOfBirth());
-        SetField('address', GenerateStreetAddress());
-        SetField('phone', GeneratePhoneNumber());
-        SetField('city', GenerateCity())
-        //let provinces = ValuesFromDropdownField('react-select-3-input');
-        //SetField('react-select-3-input', provinces[Math.floor(Math.random() * provinces.length)]);
-        SetField('postal-code', GeneratePostalCode());
-
-        // Vehicle information section
-        //let vehicleJurisdictions = ValuesFromDropdownField('react-select-2-input');
-        //SetField('react-select-4-input', licenceJurisdictions[Math.floor(Math.random() * vehicleJurisdictions.length)]);
-        SetField('plate-number', GenerateLicencePlate());
-        SetField('registration-number', GenerateRegistrationNumber());
-        SetField('vin-number', GenerateVIN());
-        SetField('nsc-number', GenerateNSC());
-        
-        // Registered owner section
-        SetField('owner-last-name', chance.last());
-        SetField('owner-first-name', GenerateGivenNames());
-        SetField('registered-owner-address', GenerateStreetAddress());
-        SetField('registered-owner-phone', GeneratePhoneNumber());
-        SetField('registered-owner-city', GenerateCity());
-        SetSelect('registered-owner-prov-state-select', "BRITISH COLUMBIA");
-        SetSelect('vehicle-year-select', "1979");
-        SetSelect('vehicle-make-model-select', "CHEVROLET - CHEVETTE");
-        SetSelect('vehicle-style-select', "5-DOOR HATCH");
-        SetSelect('vehicle-colour', "YELLOW");
-        
-        // Vehicle impoundment or disposition
-        // Radio button vehicle-impounded-YES
-        SelectRadioButton('vehicle-impounded-YES');
-        SelectRadioButton('key-location-WITH VEHICLE');
-        SetField('ILO-options-select', 'COLD COUNTRY TOWING (CRANBROOK), 3584 COLLINSON RD, CRANBROOK, 250-426-3680');
-        
-        // Prohibition
-        SelectRadioButton('type-of-prohibition-alcohol');
-        SetField('offence-address', GenerateStreetAddress());
-        SetField('offence-city-select', GenerateCity())
-        SetField('offence-agency-file', "RSI" + GenerateRandomNumberLength(4));
-
-        SetField('registered-owner-postal', GeneratePostalCode());
-        SetField('date-of-driving', GetCurrentDate());
-        SetField('time-of-driving', GetTimeFiveMinutesAgo())
-
-        // Reasonable grounds
-        SelectRadioButton('witnessed-by-officer');
-        SelectRadioButton('independent-witness');
-        SelectRadioButton('prescribed-device-YES');
-
-        // Officer section
-        SetField('officer-lastname', chance.last());
-        SetField('officer-prime-id', "QA" + GenerateRandomNumberLength(4));
-        SetField('officer-agency', GenerateCity() + " COPS");        
-        SetField('date-of-test', GetCurrentDate());
-        SetField('time-of-test', GetTimeFiveMinutesAgo());
-        SelectRadioButton('test-used-alcohol-physical-cordination-test')
-     })
-    //document.body.insertAdjacentElement("afterbegin", btn);
-    document.getElementById("qa-automation").appendChild(btn);
-}
-
 
 // Select the entire DOM for observing:
 const target = document.querySelector('body');
@@ -114,3 +37,176 @@ const config = {childList: true};
 
 // Start the observer
 observer.observe(target, config);
+
+// Helper function to overlay an ugly Fill button on the form
+function AddButton(buttonName, topLocation, leftLocation, zIndex, fieldStructure) 
+{
+    let btn = document.createElement("button");
+    btn.innerHTML = buttonName;
+    buttonStyle = "top:" + topLocation + " !important;left:" + leftLocation + " !important;position:fixed;z-index: " + zIndex;
+    buttonStyle += ";background-color: #ffff00 !important;";
+    btn.style = buttonStyle;
+
+    btn.addEventListener('click', () => {
+        FillDriverSection();
+        FillVehicleSection();
+        FillOwnerSection();
+        FillVehicleImpoundmentSection();
+        FillProhibitionSection();
+        FillImpoundmentForIrpSection();
+        FillImpoundmentForDrivingBehavhiour();
+        FillUnlicencedDriverSection();
+        FillExcessiveSpeedSection();
+        FillLinkageFactorsSection();
+        FillIncidentDetailsSection();
+        FillReasonableGroundsSection();
+        FillTestAdministeredSection();
+        FillOfficerSection();
+     })
+
+    // Add the button to the page
+    document.getElementById("qa-automation").appendChild(btn);
+}
+
+function FillDriverSection() {
+    SetField('drivers_licence_no', GenerateDL());
+    SetField('driver_last_name', chance.last());
+    SetField('driver_given_name', GenerateGivenNames());
+    SetField('driver_dob', GenerateDateOfBirth());
+    SetField('driver_address', GenerateStreetAddress());
+    SetField('driver_phone', GeneratePhoneNumber());
+    SetField('driver_city', GenerateCity())
+    SetField('driver_postal', GeneratePostalCode());
+    SetField('gender', RandomGender());
+    SetField('driver_licence_expiry', GenerateDLExpiryDate());
+    SetField('driver_licence_class', GenerateRandomDlClass());
+}
+
+function FillVehicleSection(){
+    SetField('vehicle_plate_no', GenerateLicencePlate());
+    SetField('vehicle_registration_no', GenerateRegistrationNumber());
+    SetField('vehicle_vin_no', GenerateVIN());
+    SetField('nsc_no', GenerateNSC());
+
+    //SetMultiSelect('vehicle_year-select', "1979");
+    SelectRandomVehicleYear('vehicle_year-select');
+    //SetMultiSelect('vehicle_mk_md-select', "CHEVROLET - CHEVETTE");
+    SelectRandomVehicle('vehicle_mk_md-select');
+    //SetMultiSelect('vehicle_style-select', "5-DOOR HATCH");
+    SelectRandomVehicleStyle('vehicle_style-select');
+    //SetMultiSelect('vehicle_colour', "YELLOW");
+    SelectRandomVehicleColour('vehicle_colour');
+}
+
+function FillOwnerSection() {
+    RandomlySelectRadioButton('owned_by_corp');
+    SetField('regist_owner_last_name', chance.last());
+    SetField('regist_owner_first_name', GenerateGivenNames());
+    SetField('regist_owner_address', GenerateStreetAddress());
+    SetField('regist_owner_phone', GeneratePhoneNumber());
+    SetField('regist_owner_city', GenerateCity());
+    SetMultiSelect('regist_owner_prov_state-select', "BRITISH COLUMBIA");
+    SetSelect('regist_owner_postal', GeneratePostalCode());
+}
+
+function FillVehicleImpoundmentSection() {
+    // Radio button vehicle-impounded-YES
+    SetField('date_of_impound', GetCurrentDate())
+    //SelectRadioButton('vehicle-impounded-YES');
+    RandomlyChooseRadio('location_of_keys-WITH VEHICLE', 'location_of_keys-WITH DRIVER');
+    //SetMultiSelect('ILO-options-select', 'COLD COUNTRY TOWING (CRANBROOK), 3584 COLLINSON RD, CRANBROOK, 250-426-3680');
+    SelectRandomIlo('ILO-options-select');
+}
+
+function FillProhibitionSection() {
+    RandomlyChooseRadio('type_of_prohibition-alcohol','type_of_prohibition-drugs');
+    SetField('intersection_or_address_of_offence', GenerateStreetAddress());
+    SetMultiSelect('offence_city-select', GenerateCity())
+    SetField('agency_file_no', "RSI" + GenerateRandomNumberLength(4));
+    SetField('date_of_driving', GetCurrentDate());
+    SetField('time_of_driving', GetTimeFiveMinutesAgo());
+}
+
+function FillImpoundmentForIrpSection() {
+    RandomlyChooseRadio('irp_impound-YES', 'irp_impound-NO');
+    RandomlyChooseRadio('irp_impound_duration-3DAY', 'irp_impound_duration-7DAY', 'irp_impound_duration-30DAY');
+    SetField('IRP_number', "01-000001");
+}
+
+function FillImpoundmentForDrivingBehavhiour() {
+    RandomlySelectRadioButton('excessive_speed');
+    RandomlySelectRadioButton('prohibited');
+    RandomlySelectRadioButton('suspended');
+    RandomlySelectRadioButton('street_racing');
+    RandomlySelectRadioButton('stunt_driving');
+    RandomlySelectRadioButton('motorcycle_seating');
+    RandomlySelectRadioButton('unlicensed');
+}
+
+function FillUnlicencedDriverSection() {
+    SetField('unlicenced_prohibition_number', "02-000002");
+    RandomlyChooseRadio('belief_driver_bc_resident-YES', 'belief_driver_bc_resident-NO');
+    RandomlyChooseRadio('out_of_province_dl-YES', 'out_of_province_dl-NO');
+    SetField('out_of_province_dl_number', GenerateDL())
+}
+
+function FillExcessiveSpeedSection() {
+    var speedLimit = RandomlySpeedLimit();
+    SetField('speed_limit', speedLimit);
+    SetField('vehicle_speed', parseInt(speedLimit) + 45);
+    RandomlyChooseRadio('speed_estimation_technique-VISUAL', 'speed_estimation_technique-PACING')
+    RandomlyChooseRadio('speed_confirmation_technique-LASER', 'speed_confirmation_technique-RADAR', 'speed_confirmation_technique-OTHER');
+}
+
+function FillLinkageFactorsSection() {
+    RandomlySelectRadioButton('linkage_location_of_keys');
+    RandomlySelectRadioButton('linkage_driver_principal');
+    RandomlySelectRadioButton('linkage_owner_in_vehicle');
+    RandomlySelectRadioButton('linkage_owner_aware_possesion');
+    RandomlySelectRadioButton('linkage_vehicle_transfer_notice');
+    RandomlySelectRadioButton('linkage_other');
+    SetField('linkage_location_of_keys_explanation', chance.paragraph({ sentences: 1 }));
+}
+
+function FillIncidentDetailsSection() {
+    RandomlySelectRadioButton('incident_details_extra_page');
+    var numberOfSentences = Math.floor(Math.random() * 10) + 10;
+    SetField('incident_details', chance.paragraph({ sentences: numberOfSentences }));
+}
+
+function FillReasonableGroundsSection() {
+    RandomlySelectRadioButton('witnessed-by-officer');
+    RandomlySelectRadioButton('admission-by-driver');
+    RandomlySelectRadioButton('independent-witness');
+    RandomlySelectRadioButton('video-surveillance');
+    RandomlySelectRadioButton('reasonable_ground_other');
+    SetField('other-reason', chance.paragraph({ sentences: 1 }));
+    RandomlyChooseRadio('prescribed_test_used-YES', 'prescribed_test_used-NO');
+    SetField('date_of_test', GetCurrentDate());
+    SetField('time_of_test', GetTimeOneMinuteAgo());
+    RandomlyChooseRadio('reason_for_not_using_prescribed_test-refused', 'reason_for_not_using_prescribed_test-opinion')
+}
+
+function FillTestAdministeredSection() {
+    // Test administered (alcohol)
+    RandomlyChooseRadio('test_used_alcohol-alco-sensor', 'test_used_alcohol-instrument', 'test_used_alcohol-physical-cordination-test');
+    SetField('asd_expiry_date', GetDateInFiveFiveDays());
+    RandomlyChooseRadio('result_alcohol-51-99 mg%', 'result_alcohol-51-99 mg%');
+    SetField('bac_result_mg', Math.floor(Math.random() * 1000));
+
+    // Test administered (drugs)
+    RandomlyChooseRadio('test_used_drugs-approved-drug', // test_used_drugs-approved-drug
+        'test_used_drugs-physical-cordination-test-sfts', // test_used_drugs-physical-cordination-test-sfts
+        'test_used_drugs-physical-cordination-test-dre'); // test_used_drugs-physical-cordination-test-dre
+    RandomlySelectRadioButton('THC');
+    RandomlySelectRadioButton('Cocaine');
+}
+
+function FillOfficerSection() {
+    SetField('officer-lastname', chance.last());
+    SetField('officer-prime-id', "QA" + GenerateRandomNumberLength(4));
+    SetField('officer-agency', GenerateCity() + " COPS");        
+    SetField('date-of-test', GetCurrentDate());
+    SetField('time-of-test', GetTimeFiveMinutesAgo());
+    SelectRadioButton('test-used-alcohol-physical-cordination-test')
+}
