b188ff360c6d38a2dbfbcca030213d4b076d25ae
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-10-13 17:08:06 -0700

    updated footer

--- roadside-forms-frontend/frontend_web_app/src/components/common/Footer/footer.js
+++ roadside-forms-frontend/frontend_web_app/src/components/common/Footer/footer.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import packageJson from '../../../../package.json';
 
 export const Footer = () => {
+  // code to on hover over <p> show another <p> element
+  const [show, setShow] = useState(false);
+  const hashValue = process.env.REACT_APP_GIT_COMMIT_HASH;
+  const showText = () => setShow(true);
+  const hideText = () => setShow(false);
+
   return (
     <footer id="roadesafety-footer" className='text-center text-font'>
-      <p>Version: {packageJson.version}</p>
+      <p onMouseOver={showText} onMouseOut={hideText}>Version: {packageJson.version}</p>
+      { show && <p>Hash: {hashValue}</p>}
     </footer>
   );
 }
