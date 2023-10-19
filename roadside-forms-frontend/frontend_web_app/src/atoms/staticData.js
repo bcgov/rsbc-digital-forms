976ed750cfc9fee47f02c089409df2427f06a601
Sam Warren <samwarren4@gmail.com>
2023-10-19 11:18:55 -0600

    format staticdata

--- roadside-forms-frontend/frontend_web_app/src/atoms/staticData.js
+++ roadside-forms-frontend/frontend_web_app/src/atoms/staticData.js
@@ -1,51 +1,51 @@
-import {atom} from 'recoil'
+import { atom } from "recoil";
 
 export const vehicles = atom({
-    key:"allVehicles",
-    default: [],
-  });
+  key: "allVehicles",
+  default: [],
+});
 
 export const vehicleStyles = atom({
-    key:"allVehicleStyles",
-    default: [],
-  });
+  key: "allVehicleStyles",
+  default: [],
+});
 
-  export const vehicleColours = atom({
-    key:"allVehicleColours",
-    default: [],
-  });
+export const vehicleColours = atom({
+  key: "allVehicleColours",
+  default: [],
+});
 
 export const provinces = atom({
-    key:"allProvinces",
-    default: [],
-  });
+  key: "allProvinces",
+  default: [],
+});
 
 export const permissions = atom({
-    key:"allPermissions",
-    default: [],
-  });
+  key: "allPermissions",
+  default: [],
+});
 
 export const jurisdictions = atom({
-    key:"allJurisdictions",
-    default: [],
-  });
+  key: "allJurisdictions",
+  default: [],
+});
 
 export const impoundLotOperators = atom({
-    key:"allImpoundLotOperators",
-    default: [],
-  });
+  key: "allImpoundLotOperators",
+  default: [],
+});
 
 export const countries = atom({
-    key:"allCountries",
-    default: [],
-  });
+  key: "allCountries",
+  default: [],
+});
 
 export const cities = atom({
-    key:"allCities",
-    default: [],
-  });
+  key: "allCities",
+  default: [],
+});
 
 export const agencies = atom({
-    key:"allAgencies",
-    default: [],
-  });
+  key: "allAgencies",
+  default: [],
+});
