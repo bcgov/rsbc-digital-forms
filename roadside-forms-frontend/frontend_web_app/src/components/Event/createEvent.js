a55d2bfbfa31b59b51d37da4f4f85bab8b8148bd
Sam Warren <samwarren4@gmail.com>
2023-11-02 17:22:59 -0600

    add missing out of province dl jurisdiction field

--- roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Event/createEvent.js
@@ -381,7 +381,9 @@ export const CreateEvent = () => {
                 <VehicleImpoundmentIRP />
                 <VehicleImpoundmentReason />
                 {values["excessive_speed"] && <Excessive />}
-                {values["unlicensed"] && <Unlicensed />}
+                {values["unlicensed"] && (
+                  <Unlicensed jurisdictions={jurisdictions} />
+                )}
                 <LinkageFactors />
                 <IncidentDetails />
               </>
--- roadside-forms-frontend/frontend_web_app/src/components/Event/validationSchema.js
+++ roadside-forms-frontend/frontend_web_app/src/components/Event/validationSchema.js
@@ -357,7 +357,4 @@ export const validationSchema = Yup.object().shape({
         return true;
       }
     ),
-  reasonable_test_used_alcohol: prescribedDeviceValidation(
-    Yup.ref("prescribed_test_used")
-  ),
 });
