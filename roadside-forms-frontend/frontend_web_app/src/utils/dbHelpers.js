58655d3d88414eeba1ee6f5d8bda010ce7ff05d1
Sam Warren <samwarren4@gmail.com>
2023-11-21 16:15:18 -0800

    fix error where form wouldn't fetch new IDs after all IDs were leased

--- roadside-forms-frontend/frontend_web_app/src/utils/dbHelpers.js
+++ roadside-forms-frontend/frontend_web_app/src/utils/dbHelpers.js
@@ -12,6 +12,7 @@ export const getAllFormIDs = async () => {
     await db.formID
       .where("form_type")
       .equals(key)
+      .and((form) => form.leased === 0)
       .toArray()
       .then((value) => {
         idDict[key] =
