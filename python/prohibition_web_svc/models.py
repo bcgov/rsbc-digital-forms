aae672742fb9c35a7038fcf0b03117daba76b3f6
Sam Warren <samwarren4@gmail.com>
2023-11-22 18:51:49 -0700

    Add form spoiling functionality

--- python/prohibition_web_svc/models.py
+++ python/prohibition_web_svc/models.py
@@ -13,21 +13,24 @@ class Form(db.Model):
     form_type: str
     lease_expiry: datetime
     printed_timestamp: datetime
+    spoiled_timestamp: datetime
     user_guid: str
     
     id = db.Column('id', db.String(20), primary_key=True)
     form_type = db.Column(db.String(20), nullable=False)
     lease_expiry = db.Column(db.Date, nullable=True)
     printed_timestamp = db.Column(db.DateTime, nullable=True)
+    spoiled_timestamp = db.Column(db.DateTime, nullable=True)
     user_guid = db.Column(db.String(80), db.ForeignKey(
         'user.user_guid'), nullable=True)
 
     # Note: The printed timestamp prior to v0.4.17 was saved in local Pacific time instead of GMT
 
-    def __init__(self, form_id, form_type, printed=None, lease_expiry=None, user_guid=None):
+    def __init__(self, form_id, form_type, printed=None, spoiled=None, lease_expiry=None, user_guid=None):
         self.id = form_id
         self.form_type = form_type
         self.printed_timestamp = printed
+        self.spoiled_timestamp = spoiled
         self.lease_expiry = lease_expiry
         self.user_guid = user_guid
 
