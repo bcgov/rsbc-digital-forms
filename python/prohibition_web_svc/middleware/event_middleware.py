21f62367033b41c7b77dd2ef0a0ba63b915e4feb
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-12-02 12:35:55 -0800

    updated event field

--- python/prohibition_web_svc/middleware/event_middleware.py
+++ python/prohibition_web_svc/middleware/event_middleware.py
@@ -79,8 +79,9 @@ def save_event_data(**kwargs) -> tuple:
             date_of_driving=data.get('date_of_driving'),
             time_of_driving=data.get('time_of_driving'),
             nsc_no=data.get('nsc_no', None),
-            nsc_prov_state=data.get('nsc_prov_state', {
-                'value': None, 'label': None}).get('value'),
+            # nsc_prov_state=data.get('nsc_prov_state', {
+            #     'value': None, 'label': None}).get('value'),
+            nsc_prov_state=(lambda x: x.get('value') if x else None)(data.get('nsc_prov_state', None)),
             owned_by_corp=data.get('owned_by_corp'),
             corporation_name=data.get('corporation_name'),
             regist_owner_last_name=data.get('regist_owner_last_name'),
