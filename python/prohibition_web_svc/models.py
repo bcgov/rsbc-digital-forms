5fc5b9756448770a827d70c2c4b0d14a1505d3a0
Amlan Chakladar <57117462+amlanc1@users.noreply.github.com>
2023-12-02 11:51:53 -0800

    updated vips api mapping

--- python/prohibition_web_svc/models.py
+++ python/prohibition_web_svc/models.py
@@ -728,4 +728,21 @@ class CityCrossRef(db.Model):
     icbc_city_name = db.Column(db.String)
     icbc_city_name_legacy = db.Column(db.String)
     vips_city_name = db.Column(db.String)
+
+@dataclass
+class ImpoundReasonCodes(db.Model):
+    __tablename__ = 'impound_reason_codes'
+
+    df_unique_code: str
+    impound_reason_name: str
+    vips_value_cd: str
+    vips_value_dsc: str
+    vips_value_abbreviated_dsc: str  
+
+    df_unique_code = db.Column(db.String, primary_key=True)
+    impound_reason_name = db.Column(db.String)
+    vips_value_cd = db.Column(db.String)
+    vips_value_dsc = db.Column(db.String)
+    vips_value_abbreviated_dsc = db.Column(db.String)
+    
     
