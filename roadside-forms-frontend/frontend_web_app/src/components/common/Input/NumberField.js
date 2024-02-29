a100f47e9ef216ceb86e092f9e207cca253037fc
Sam Warren <samwarren4@gmail.com>
2024-02-29 13:52:58 -0700

    Make NSC number field numeric only

---
+++ roadside-forms-frontend/frontend_web_app/src/components/common/Input/NumberField.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useField } from "formik";
+
+export const NumberField = ({ required, label, name, ...props }) => {
+  const [field, meta] = useField(name);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    // Limit the input value to 999
+    const sanitizedValue = value.replace(/[^0-9]/g, "");
+
+    // Update the field value
+    field.onChange({ target: { name, value: sanitizedValue } });
+  };
+
+  return (
+    <div>
+      <label htmlFor={name}>
+        {label}
+        {required && <span className="required-asterisk"> *</span>}
+      </label>
+      <input
+        type="text"
+        id={name}
+        {...field}
+        {...props}
+        onChange={handleChange}
+        required={required}
+        className={`form-control ${meta.touched && meta.error && "is-invalid"}`}
+        autoComplete="off"
+      />
+      {meta.touched && meta.error && (
+        <div className="invalid-feedback">{meta.error}</div>
+      )}
+    </div>
+  );
+};
