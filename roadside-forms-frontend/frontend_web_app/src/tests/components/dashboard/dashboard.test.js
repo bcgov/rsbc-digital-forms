06cbcc5eb81113a85bfd6c9064a5afaaf325fb93
Adimar Borges <adimar.borges@aot-technologies.com>
2024-05-27 12:03:37 -0700

    Update unit tests

--- roadside-forms-frontend/frontend_web_app/src/tests/components/dashboard/dashboard.test.js
+++ roadside-forms-frontend/frontend_web_app/src/tests/components/dashboard/dashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import { Dashboard } from '../../../components/Dashboard/Dashboard';
 import { StaticDataApi } from '../../../api/staticDataApi';
@@ -10,80 +10,107 @@ jest.mock('react-router-dom', () => ({
     useNavigate: jest.fn(),
   }));
 describe('Dashboard component', () => {
+    beforeAll(() => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date("2024-01-01T00:00"));
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
     test('renders dashboard component correctly', async () => {
-    StaticDataApi.get = jest.fn().mockResolvedValue({});
+    StaticDataApi.get = jest.fn().mockImplementation(async (table) => {
+        switch (table) {
+            case "vehicle_styles":
+                return { data: [{
+                    "code": "2DR",
+                    "name": "2-DOOR SEDAN"
+                },
+                {
+                    "code": "3DR",
+                    "name": "3-DOOR HATCH"
+                }]};
+            case "jurisdictions":
+                return { data: [{
+                    "id": 1,
+                    "objectCd": "AL",
+                    "objectDsc": "ALABAMA"
+                },
+                {
+                    "id": 2,
+                    "objectCd": "AK",
+                    "objectDsc": "ALASKA"
+                }]};
+            case "provinces":
+                return { data: [{
+                    "id": 1,
+                    "objectCd": "AK",
+                    "objectDsc": "ALASKA"
+                },
+                {
+                    "id": 2,
+                    "objectCd": "ALTA",
+                    "objectDsc": "ALBERTA"
+                }]};
+            case "vehicles":
+                return { data: [{
+                    "id": 123,
+                    "mk": "AC",
+                    "search": "A C (GREAT BRITAIN)",
+                    "md": ""
+                },
+                {
+                    "id": 124,
+                    "mk": "AC",
+                    "search": "A C (GREAT BRITAIN) - 3000 ME",
+                    "md": "300"
+                }]};
+            case "vehicle_colours":
+                return { data: [
+                    {"code": 'test', 
+                    "display_name": 'test'}
+                ]};
+            case "cities":
+                return { data: [{
+                    "id": 1,
+                    "objectCd": "OH3H",
+                    "objectDsc": "100 TEST 1 HOUSE"
+                },
+                {
+                    "id": 2,
+                    "objectCd": "OHdM",
+                    "objectDsc": "106 TEST HOUSE"
+                }]};
+            case "impound_lot_operators":
+                return { data: [
+                {
+                    "id": 1,
+                    "name": "Tset",
+                    "lot_address": "Test ST",
+                    "city": "TEST",
+                    "phone": "250-000-5343"
+                },
+                {
+                    "id": 2,
+                    "name": "Tset",
+                    "lot_address": "Test ST",
+                    "city": "TEST",
+                    "phone": "250-000-5343"
+                }]};
+            default:
+                return {data: []};
+        }
+    });
     FormSubmissionApi.get = jest.fn().mockResolvedValue({});
     
-    let container; 
-
+    let container;
     await act(async () => {
-      ({ container } = render( 
-        <RecoilRoot
-          initializeState={(snap) => {
-            snap.set(vehicleStyles, [  {
-                "code": "2DR",
-                "name": "2-DOOR SEDAN"
-            },
-            {
-                "code": "3DR",
-                "name": "3-DOOR HATCH"
-            }]);
-            snap.set(jurisdictions, [{
-                "objectCd": "AL",
-                "objectDsc": "ALABAMA"
-            },
-            {
-                "objectCd": "AK",
-                "objectDsc": "ALASKA"
-            }]);
-            snap.set(provinces, [{
-                "objectCd": "AK",
-                "objectDsc": "ALASKA"
-            },
-            {
-                "objectCd": "ALTA",
-                "objectDsc": "ALBERTA"
-            }]);
-            snap.set(vehicles, [{
-                "mk": "AC",
-                "search": "A C (GREAT BRITAIN)",
-                "md": ""
-            },
-            {
-                "mk": "AC",
-                "search": "A C (GREAT BRITAIN) - 3000 ME",
-                "md": "300"
-            }]);
-            snap.set(vehicleColours, [
-                {"code": 'test', 
-                "display_name": 'test'}
-            ]);
-            snap.set(cities, [{
-                "objectCd": "OH3H",
-                "objectDsc": "100 TEST 1 HOUSE"
-            },
-            {
-                "objectCd": "OHdM",
-                "objectDsc": "106 TEST HOUSE"
-            }]);
-            snap.set(impoundLotOperators, [
-            {
-                "name": "Tset",
-                "lot_address": "Test ST",
-                "city": "TEST",
-                "phone": "250-000-5343"
-            },
-            {
-                "name": "Tset",
-                "lot_address": "Test ST",
-                "city": "TEST",
-                "phone": "250-000-5343"
-            }]);
-          }}
-        >
-          <Dashboard />
-        </RecoilRoot>
-      ));
+        ({ container } = render( 
+            <RecoilRoot>
+            <Dashboard />
+            </RecoilRoot>
+        ));
     });
 
     expect(container).toMatchSnapshot();
--- roadside-forms-frontend/frontend_web_app/src/tests/components/event/createEvent.test.js
+++ roadside-forms-frontend/frontend_web_app/src/tests/components/event/createEvent.test.js
@@ -1,87 +1,101 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import { CreateEvent } from '../../../components/Event/createEvent';
 import { vehicleStyles, jurisdictions, provinces, vehicles, vehicleColours, cities, impoundLotOperators } from '../../../atoms/staticData';
+import { useBlocker, useLocation, useNavigate } from 'react-router-dom';
+import { useKeycloak } from '@react-keycloak/web';
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useNavigate: jest.fn(),
-  }));
+    useBlocker: jest.fn(),
+    useLocation: jest.fn(),
+}));
+jest.mock('@react-keycloak/web', () => ({
+    ...jest.requireActual('@react-keycloak/web'),
+    useKeycloak: jest.fn(),
+}));
+
+const mockNavigate = jest.fn();
+const mockKeycloak = jest.fn();
+const mockBlocker = jest.fn();
 describe('CreateEvent component', () => {
     test('renders create event component correctly', async () => {
-    let container; 
+        
+        useNavigate.mockImplementation(() => mockNavigate);
+        useKeycloak.mockImplementation(() => mockKeycloak);
+        useBlocker.mockImplementation(() => mockBlocker);
+        useLocation.mockImplementation(() => ({}));
 
-    await act(async () => {
-      ({ container } = render( 
-        <RecoilRoot
-          initializeState={(snap) => {
-            snap.set(vehicleStyles, [  {
-                "code": "2DR",
-                "name": "2-DOOR "
-            },
-            {
-                "code": "3DR",
-                "name": "3-DOOR "
-            }]);
-            snap.set(jurisdictions, [{
-                "objectCd": "AL",
-                "objectDsc": "ALABAMA"
-            },
-            {
-                "objectCd": "AK",
-                "objectDsc": "ALASKA"
-            }]);
-            snap.set(provinces, [{
-                "objectCd": "AK",
-                "objectDsc": "ALASKA"
-            },
-            {
-                "objectCd": "ALTA",
-                "objectDsc": "ALBERTA"
-            }]);
-            snap.set(vehicles, [{
-                "mk": "AC-TEST",
-                "search": "A C (GREAT BRITAIN)",
-                "md": ""
-            },
-            {
-                "mk": "TEST",
-                "search": "A C (GREAT BRITAIN) - 3000 ME",
-                "md": "300"
-            }]);
-            snap.set(vehicleColours, [
-                {"code": 'test', 
-                "display_name": 'test'}
-            ]);
-            snap.set(cities, [{
-                "objectCd": "TEST",
-                "objectDsc": "10 TEST HOUSE"
-            },
-            {
-                "objectCd": "OHTM",
-                "objectDsc": "10390 TEST ST"
-            }]);
-            snap.set(impoundLotOperators, [
-            {
-                "name": "TSET",
-                "lot_address": "7 TEST ST",
-                "city": "TEST",
-                "phone": "250-003-73654"
-            },
-            {
-                "name": "TSET",
-                "lot_address": "7 TEST ST",
-                "city": "TEST",
-                "phone": "250-003-73654"
-            }]);
-          }}
-        >
-          <CreateEvent />
-        </RecoilRoot>
-      ));
+        const {container} = render( 
+            <RecoilRoot
+            initializeState={(snap) => {
+                snap.set(vehicleStyles, [  {
+                    "code": "2DR",
+                    "name": "2-DOOR "
+                },
+                {
+                    "code": "3DR",
+                    "name": "3-DOOR "
+                }]);
+                snap.set(jurisdictions, [{
+                    "objectCd": "AL",
+                    "objectDsc": "ALABAMA"
+                },
+                {
+                    "objectCd": "AK",
+                    "objectDsc": "ALASKA"
+                }]);
+                snap.set(provinces, [{
+                    "objectCd": "AK",
+                    "objectDsc": "ALASKA"
+                },
+                {
+                    "objectCd": "ALTA",
+                    "objectDsc": "ALBERTA"
+                }]);
+                snap.set(vehicles, [{
+                    "mk": "AC-TEST",
+                    "search": "A C (GREAT BRITAIN)",
+                    "md": ""
+                },
+                {
+                    "mk": "TEST",
+                    "search": "A C (GREAT BRITAIN) - 3000 ME",
+                    "md": "300"
+                }]);
+                snap.set(vehicleColours, [
+                    {"code": 'test', 
+                    "display_name": 'test'}
+                ]);
+                snap.set(cities, [{
+                    "objectCd": "TEST",
+                    "objectDsc": "10 TEST HOUSE"
+                },
+                {
+                    "objectCd": "OHTM",
+                    "objectDsc": "10390 TEST ST"
+                }]);
+                snap.set(impoundLotOperators, [
+                {
+                    "name": "TSET",
+                    "lot_address": "7 TEST ST",
+                    "city": "TEST",
+                    "phone": "250-003-73654"
+                },
+                {
+                    "name": "TSET",
+                    "lot_address": "7 TEST ST",
+                    "city": "TEST",
+                    "phone": "250-003-73654"
+                }]);
+            }}
+            >
+            <CreateEvent />
+            </RecoilRoot>
+        );
+
+        expect(container).toMatchSnapshot();
     });
-
-    expect(container).toMatchSnapshot();
-});
 });
