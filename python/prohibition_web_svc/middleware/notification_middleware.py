c87c0f2deb27b043be76311ed58759d106c2ee08
Ratheesh kumar R <108045773+ratheesh-aot@users.noreply.github.com>
2024-09-09 14:40:11 -0700

    [DF-3071] Enhancement for Digital Forms Admin Page  (#370)
    * DF-3071: Admin form changes
    
    Added filter and sort for columns
    Added pagination
    Added checkbox filter for New user request only
    Added last active column
    
    * DF-3071: Uninstalled overlay
    
    * DF-3071: Updated front end unit test
    
    * DF-3071: Admin forms backend changes
    
    Email Notification when a new user sends access request to DF forms
    Create a new field to identify the last active date of a user
    
    * [DF-3071] - Added new ENV REACT_APP_BASE_URL
    
    * Snapshot restored
    
    * [DF-3071] - Unit test to increase coverage
    
    * DF-3071: Improve front end code coverage
    
    * fix python requirements
    
    * Updating front end test config
    
    * Fixing jest config
    
    * Fixed snapshot

---
+++ python/prohibition_web_svc/middleware/notification_middleware.py
@@ -0,0 +1,18 @@
+from python.prohibition_web_svc.config import Config
+import python.common.rsi_email as rsi_email
+
+
+def send_new_user_admin_notification(**kwargs):
+    subject = "Digital Forms: New User Application"
+    body = "A new user has applied for access to Digital Forms:"
+    message = {
+        "first_name": kwargs.get('payload')['first_name'],
+        "last_name": kwargs.get('payload')['last_name'],
+        "badge_number": kwargs.get('payload')['badge_number'],
+        "agency": kwargs.get('payload')['agency'],
+        "admin_link": Config.REACT_APP_BASE_URL + "/admin-console",
+    }
+
+    rsi_email.send_new_user_admin_notification(config=Config, subject=subject, body=body, message=message)
+
+    return True, kwargs
