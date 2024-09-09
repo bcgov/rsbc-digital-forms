40151a2f6c4f9f82e2282bf8f63ac473024118f2
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2024-09-09 09:50:24 -0700

    Updating front end test config

--- roadside-forms-frontend/frontend_web_app/jest.config.js
+++ roadside-forms-frontend/frontend_web_app/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     // Other Jest configuration options...
-  
+    "jest": {
+    "moduleFileExtensions": ["js", "jsx", "json", "node"]
+  },
     transform: {
       "^.+\\.js$": "babel-jest",
     },
