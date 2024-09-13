412618e677bd5ec59a9d3678e7b17afd93abc8ba
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2024-09-13 09:30:16 -0700

    Record errors happening while creating event or event pdf

--- python/common/models.py
+++ python/common/models.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
+from python.common.enums import ErrorCategory, ErrorSeverity, ErrorStatus, ErrorCode, EventType
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -790,3 +791,28 @@ class IloIdCrossRef(db.Model):
     vips_impound_lot_operator_id = db.Column(db.Integer)
     
     
+@dataclass
+class DFErrors(db.Model):
+    __tablename__ = 'df_errors'
+
+    error_id: int = db.Column(db.Integer, primary_key=True)
+    error_cd: ErrorCode = db.Column(db.Enum(ErrorCode), nullable=False)
+    error_cd_desc: str = db.Column(db.String(200), nullable=False)
+    error_category_cd: ErrorCategory = db.Column(db.Enum(ErrorCategory), nullable=False)
+    error_severity_level_cd: ErrorSeverity = db.Column(db.Enum(ErrorSeverity), nullable=False)
+    error_details: str = db.Column(db.Text)
+    error_path: str = db.Column(db.String(200))
+    event_id: int = db.Column(db.Integer, db.ForeignKey('event.event_id'), nullable=True)
+    event_type: EventType = db.Column(db.Enum(EventType), nullable=True)
+    ticket_no: str = db.Column(db.String(50))
+    received_dt: datetime = db.Column(db.DateTime, default=datetime.now())
+    error_status_cd: ErrorStatus = db.Column(db.Enum(ErrorStatus), default=ErrorStatus.NEW)
+    req_payload: str = db.Column(db.Text)
+    
+    created_by: str = db.Column(db.String(150))
+    created_dt: datetime = db.Column(db.DateTime, default=datetime.now())
+    updated_by: str = db.Column(db.String(150))
+    updated_dt: datetime = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def __repr__(self):
+        return f'<DF_Error {self.error_id}: {self.error_category_cd.name} - {self.error_severity_level_cd.name}>'
