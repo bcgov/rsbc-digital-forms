23980e714e1eba3b570c78df72b61d1d80041eb0
Adimar Borges <adimar.borges@aot-technologies.com>
2025-03-25 16:19:00 -0700

    fix unit test

--- roadside-forms-frontend/frontend_web_app/jest.config.js
+++ roadside-forms-frontend/frontend_web_app/jest.config.js
@@ -1,9 +1,15 @@
+const { transform } = require("lodash");
+
 module.exports = {
     // Other Jest configuration options...
     moduleFileExtensions: ["js", "jsx", "json", "node"],
     transform: {
       "^.+\\.js$": "babel-jest",
     },
+    transformIgnorePatterns: [
+      "node_modules/(?!(.*.mjs$|keycloak.js))",
+      "node_modules/(?!(react|@react-keycloak|@react-keycloak/web)/)"
+    ],
     "setupFiles": [
       "fake-indexeddb/auto",
       "./src/setupTests.js"
