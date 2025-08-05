5dbcb7f70a19f2446476fa3807d66e9075f680c3
Adimar Borges <adimar.borges@aot-technologies.com>
2025-08-05 09:37:22 -0700

    refactor agency and crossref

--- python/common/models/agency_crossref.py
+++ python/common/models/agency_crossref.py
@@ -6,19 +6,26 @@ class AgencyCrossref(db.Model):
     __tablename__ = 'agency_cross_refs'
 
     agency_name: str
-    agency_id: str
+    agency_id: int
     agency_city: str
     prime_vjur: str
     icbc_detachment_name: str
     icbc_city_name: str
     vips_policedetachments_agency_id: str
     vips_policedetachments_agency_nm: str
-
-    agency_name = db.Column(db.String, primary_key=True)
-    agency_id = db.Column(db.String)
+    prime_agency: str
+    hidden_tf: bool = False
+    hidden_reason: str
+    
+    agency_name = db.Column(db.String, nullable=False)
+    agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'), primary_key=True, nullable=False)
     agency_city = db.Column(db.String)
     prime_vjur = db.Column(db.String)
     icbc_detachment_name = db.Column(db.String)
     icbc_city_name = db.Column(db.String)
     vips_policedetachments_agency_id = db.Column(db.String)
     vips_policedetachments_agency_nm = db.Column(db.String)
+    prime_agency = db.Column(db.String(50))
+    hidden_tf = db.Column(db.Boolean, default=False)
+    hidden_reason = db.Column(db.String(50))
+
