3504a6f7f1221ef2fbef04e20ff76d480d19ec4e
Adimar Borges <adimar.borges@aot-technologies.com>
2025-08-18 12:05:22 -0700

    update collision mapper to avoid errors with extra fields

--- python/prohibition_web_svc/mappers/collision_mapper.py
+++ python/prohibition_web_svc/mappers/collision_mapper.py
@@ -106,8 +106,118 @@ class CollisionMapper:
         """
         Maps the witnesses part of CollisionRequestPayload to a list of TarWitnessInfo objects.
         """
-        # Assuming collision_request_payload['witnesses'] is a list of witness data
-        return [TarWitnessInfo(**witness) for witness in collision_request_payload.get('witnesses', [])]
+        result = []
+        for witness in collision_request_payload.get('witnesses', []):
+            tar_witness = TarWitnessInfo(
+            collision_case_num=witness.get('collision_case_num'),
+            witness_name=witness.get('witness_name'),
+            address=witness.get('address'),
+            contact_phn_num=witness.get('contact_phn_num')
+            )
+            result.append(tar_witness)
+        return result
+    
+    @staticmethod
+    def map_to_tar_involved_person(person_data: dict) -> TarInvolvedPerson:
+        """
+        Maps a person dictionary to a TarInvolvedPerson object field by field.
+        """
+        return TarInvolvedPerson(
+            status=person_data.get('status'),
+            surname=person_data.get('surname'),
+            given_name=person_data.get('given_name'),
+            vehicle_occupied=person_data.get('vehicle_occupied'),
+            position_of_person=person_data.get('position_of_person'),
+            safety_equipment_used=person_data.get('safety_equipment_used'),
+            ejection_from_vehicle=person_data.get('ejection_from_vehicle'),
+            age=person_data.get('age'),
+            sex=person_data.get('sex'),
+            severe_injury_location=person_data.get('severe_injury_location'),
+            injury_type=person_data.get('injury_type'),
+            consciousness_state=person_data.get('consciousness_state'),
+            injured_taken_to=person_data.get('injured_taken_to'),
+            injured_taken_by=person_data.get('injured_taken_by'),
+            injury_classification=person_data.get('injury_classification'),
+            date_of_death=person_data.get('date_of_death')
+        )
+    
+    @staticmethod
+    def map_to_tar_charges(charge_data: dict) -> TarCharges:
+        """
+        Maps a charge dictionary to a TarCharges object field by field.
+        """
+        return TarCharges(
+            charge_id=charge_data.get('charge_id'),
+            charge_type=charge_data.get('charge_type'),
+            section_num=charge_data.get('section_num'),
+            offence_title=charge_data.get('offence_title')
+        )
+    
+    @staticmethod
+    def map_to_tar_entity(entity_data: dict) -> TarEntity:
+        """
+        Maps an entity dictionary to a TarEntity object field by field.
+        """
+        return TarEntity(
+            collision_case_num=entity_data.get('collision_case_num'),
+            entity_type=entity_data.get('entity_type'),
+            entity_num=entity_data.get('entity_num'),
+            possible_offender=entity_data.get('possible_offender'),
+            vehicle_parked=entity_data.get('vehicle_parked'),
+            unknown_entity=entity_data.get('unknown_entity'),
+            driver_license_num=entity_data.get('driver_license_num'),
+            license_prov_state=entity_data.get('license_prov_state'),
+            license_expiry=entity_data.get('license_expiry'),
+            surname=entity_data.get('surname'),
+            given_name=entity_data.get('given_name'),
+            license_class=entity_data.get('license_class'),
+            graduated_license_type=entity_data.get('graduated_license_type'),
+            residential_address=entity_data.get('residential_address'),
+            business_address=entity_data.get('business_address'),
+            business_phone_num=entity_data.get('business_phone_num'),
+            date_of_birth=entity_data.get('date_of_birth'),
+            age_at_collision=entity_data.get('age_at_collision'),
+            contact_phone_num=entity_data.get('contact_phone_num'),
+            sex=entity_data.get('sex'),
+            contributing_factor_1=entity_data.get('contributing_factor_1'),
+            contributing_factor_2=entity_data.get('contributing_factor_2'),
+            contributing_factor_3=entity_data.get('contributing_factor_3'),
+            contributing_factor_4=entity_data.get('contributing_factor_4'),
+            charges_blood_alc_tests_taken=entity_data.get('charges_blood_alc_tests_taken'),
+            blood_alc_test=entity_data.get('blood_alc_test'),
+            result_1=entity_data.get('result_1'),
+            result_2=entity_data.get('result_2'),
+            vehicle_plate_num=entity_data.get('vehicle_plate_num'),
+            vehicle_plate_prov_state=entity_data.get('vehicle_plate_prov_state'),
+            vehicle_year=entity_data.get('vehicle_year'),
+            vehicle_make=entity_data.get('vehicle_make'),
+            vehicle_style=entity_data.get('vehicle_style'),
+            vehicle_colour=entity_data.get('vehicle_colour'),
+            trailer_towed_plate_num=entity_data.get('trailer_towed_plate_num'),
+            trailer_towed_plate_prov_state=entity_data.get('trailer_towed_plate_prov_state'),
+            is_registered_owner=entity_data.get('is_registered_owner'),
+            vehicle_owner_name=entity_data.get('vehicle_owner_name'),
+            vehicle_owner_address=entity_data.get('vehicle_owner_address'),
+            nsc_num=entity_data.get('nsc_num'),
+            jur_code=entity_data.get('jur_code'),
+            damage_location_code=entity_data.get('damage_location_code'),
+            severety_code=entity_data.get('severety_code'),
+            estimated_vehicle_damage=entity_data.get('estimated_vehicle_damage'),
+            vehicle_stolen=entity_data.get('vehicle_stolen'),
+            vehicle_towed=entity_data.get('vehicle_towed'),
+            vehicle_towed_by=entity_data.get('vehicle_towed_by'),
+            dir_of_travel=entity_data.get('dir_of_travel'),
+            entity_street=entity_data.get('entity_street'),
+            insurance_coverage=entity_data.get('insurance_coverage'),
+            other_insurer=entity_data.get('other_insurer'),
+            other_insurance_policy_num=entity_data.get('other_insurance_policy_num'),
+            second_contact=entity_data.get('second_contact'),
+            pre_collision_vehicle_action_first_event=entity_data.get('pre_collision_vehicle_action_first_event'),
+            vehicle_type=entity_data.get('vehicle_type'),
+            vehicle_use=entity_data.get('vehicle_use'),
+            involved_persons=entity_data.get('involved_persons'),
+            charges=entity_data.get('charges')
+        )
     
     @staticmethod
     def map_to_tar_entities(collision_request_payload: CollisionRequestPayload) -> list:
@@ -117,10 +227,10 @@ class CollisionMapper:
         # Assuming collision_request_payload['entities'] is a list of entity data
         result = []
         for entity in collision_request_payload.get('entities', []):
-            involved_persons = [TarInvolvedPerson(**person) for person in entity.get('involved_persons', [])]
-            charges = [TarCharges(**charge) for charge in entity.get('charges', [])]
+            involved_persons = [CollisionMapper.map_to_tar_involved_person(person) for person in entity.get('involved_persons', [])]
+            charges = [CollisionMapper.map_to_tar_charges(charge) for charge in entity.get('charges', [])]
             entity['involved_persons'] = involved_persons
             entity['charges'] = charges
             # Create TarEntity object with the updated entity data
-            result.append(TarEntity(**entity))
+            result.append(CollisionMapper.map_to_tar_entity(entity))
         return result
