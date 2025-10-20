f72f0d66d026516788ee4fbe5544d820ea754d28
jaise-aot <jaise.thomas@aot-technologies.com>
2025-10-20 13:00:50 -0700

    file upload - upload files with admin role

---
+++ python/prohibition_web_svc/blueprints/files.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import CORS
+
+from python.common.logging_utils import get_logger
+import python.common.helper as helper
+
+from python.prohibition_web_svc.config import Config
+from python.prohibition_web_svc.middleware import files_middleware
+import python.prohibition_web_svc.business.keycloak_logic as keycloak_logic
+import python.prohibition_web_svc.http_responses as http_responses
+
+logger = get_logger(__name__)
+bp = Blueprint('files', __name__, url_prefix=Config.URL_PREFIX + '/api/v1')
+CORS(bp, resources={Config.URL_PREFIX + "/api/v1/files/*": {"origins": Config.ACCESS_CONTROL_ALLOW_ORIGIN}})
+
+@bp.route('/files', methods=['POST'])
+def create_file():
+    kwargs = helper.middle_logic(
+        keycloak_logic.get_authorized_keycloak_user() + [
+            {"try": files_middleware.upload_file, "fail": [
+                {"try": http_responses.server_error_response, "fail": []},
+            ]},
+        ],
+        required_permission='admin_user_roles-create',
+        request=request,
+        config=Config
+    )
+
+    response = kwargs.get('response')
+    status = kwargs.get('status', 200)
+    return response, status
+
+
+@bp.route('/files/<path:filename>', methods=['GET'])
+def download_file(filename):
+    return files_middleware.get_file_stream(filename)
+
+@bp.route('/files/url/<path:filename>', methods=['GET'])
+def presigned_url(filename):
+    expiry = int(request.args.get('expiry', 3600))
+    return files_middleware.generate_presigned_url(filename, expiry)
+
+@bp.route('/files', methods=['GET'])
+def list_all_files():
+    prefix = request.args.get('prefix', '')
+    return files_middleware.list_files(prefix)
+
+@bp.route('/files/<path:filename>', methods=['DELETE'])
+def remove_file(filename):
+    return files_middleware.delete_file(filename)
