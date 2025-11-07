fb8d738d19d141288a4ab08771b19403707ab6ee
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2025-11-07 13:17:04 -0800

    DF-3381 - DF Legacy Banner

--- roadside-forms-frontend/frontend_web_app/src/components/common/Layout/Layout.js
+++ roadside-forms-frontend/frontend_web_app/src/components/common/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Header } from "../Header/Header";
 import { Outlet } from "react-router";
 import { Footer } from "../Footer/footer";
+import { RetirementBanner } from "../Banner/RetirementBanner";
 
 export const Layout = () => {
   return (
     <div>
       <Header />
+      <RetirementBanner />
       <Outlet />
       <Footer />
     </div>
