afcbdcc1137b37f7cc8e863a38a9f68cdf63a15a
Adimar Borges <adimar.borges@aot-technologies.com>
2026-06-25 09:00:06 -0700

    Automatically redirect to new site

--- roadside-forms-frontend/frontend_web_app/src/components/common/Banner/RetirementBanner.js
+++ roadside-forms-frontend/frontend_web_app/src/components/common/Banner/RetirementBanner.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+
 import "./retirementBanner.scss";
 
+const REDIRECT_URL = "https://jag.gov.bc.ca/digital-forms/";
+const REDIRECT_DELAY_SECONDS = 5;
+
 export const RetirementBanner = () => {
+  const [countdown, setCountdown] = useState(REDIRECT_DELAY_SECONDS);
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setCountdown((currentCountdown) => {
+        if (currentCountdown <= 1) {
+          window.clearInterval(intervalId);
+          return 0;
+        }
+
+        return currentCountdown - 1;
+      });
+    }, 1000);
+
+    const timeoutId = window.setTimeout(() => {
+      window.location.assign(REDIRECT_URL);
+    }, REDIRECT_DELAY_SECONDS * 1000);
+
+    return () => {
+      window.clearInterval(intervalId);
+      window.clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <div className="retirement-banner" role="alert">
       <div className="retirement-banner-content">
@@ -14,9 +43,13 @@ export const RetirementBanner = () => {
               Please proceed to the new Digital Forms website to submit a Digital Form. 
               Copies of submitted forms can be requested from ICBC (12h/24h) or RSBC (VI).
             </p>
+            <p className="retirement-banner-redirect-notice">
+              You will be redirected automatically in <strong>{countdown}</strong>{" "}
+              second{countdown === 1 ? "" : "s"}.
+            </p>
             <div className="retirement-banner-actions">
               <a 
-                href="https://jag.gov.bc.ca/digital-forms/" 
+                href={REDIRECT_URL}
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="retirement-banner-button"
@@ -25,8 +58,8 @@ export const RetirementBanner = () => {
               </a>
             </div>            
             <p>Please update your Digital Forms website shortcut or bookmark:</p>
-            <a href="https://jag.gov.bc.ca/digital-forms/" target="_blank" rel="noopener noreferrer">
-              https://jag.gov.bc.ca/digital-forms/
+            <a href={REDIRECT_URL} target="_blank" rel="noopener noreferrer">
+              {REDIRECT_URL}
             </a>
           </div>
         </div>
